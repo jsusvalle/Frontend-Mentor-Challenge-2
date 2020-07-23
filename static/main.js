86183d1aac648ea69ce4b1566aef010b366d263a
Jesus Valle <58391098+jsusvalle@users.noreply.github.com>
2020-07-23 15:06:50 -0500

    Update main.js

--- static/main.js
+++ static/main.js
@@ -1,7 +1,4 @@
 const checkbox = document.querySelector('#checkbox');
-if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
-    checkbox.setAttribute('checked', true);
-}
 
 checkbox.addEventListener('change', function(evt) {
     document.body.classList.toggle('is-dark-mode');
